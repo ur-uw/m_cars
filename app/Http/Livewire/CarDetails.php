2632386a84d4876531f92c8269de1c89ff45e781
Mohammed Fadhil <mfanha8030@gmail.com>
2022-01-03 16:15:44 +0300

    [feat]: implement car images in car details screen

--- app/Http/Livewire/CarDetails.php
+++ app/Http/Livewire/CarDetails.php
@@ -8,10 +8,16 @@ use Livewire\Component;
 class CarDetails extends Component
 {
     public Car $car;
+    public $selected_car_image;
     public function mount(Car $car)
     {
+        if ($car->images != null) {
+            $this->selected_car_image = $car->images[0];
+        }
         $this->car = $car;
     }
+
+
     public function render()
     {
         return view('livewire.car-details')
