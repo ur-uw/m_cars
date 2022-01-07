00300ecacb038ef3bfdb94103c3db42839203a60
Mohammed Fadhil <mfanha8030@gmail.com>
2022-01-07 16:50:27 +0300

    [chore]: rename laravel components to be more general

--- app/Http/Livewire/ProductCard.php
+++ app/Http/Livewire/ProductCard.php
@@ -5,11 +5,11 @@ namespace App\Http\Livewire;
 use App\Models\SparePart;
 use Livewire\Component;
 
-class SparePartCard extends Component
+class ProductCard extends Component
 {
     public SparePart $spare;
     public function render()
     {
-        return view('livewire.spare-part-card');
+        return view('livewire.product-card');
     }
 }
--- app/Http/Livewire/ProductTypeCard.php
+++ app/Http/Livewire/ProductTypeCard.php
@@ -4,12 +4,12 @@ namespace App\Http\Livewire;
 
 use Livewire\Component;
 
-class SpareTypeCard extends Component
+class ProductTypeCard extends Component
 {
     public $spare_type_name;
     public $spare_type_image;
     public function render()
     {
-        return view('livewire.spare-type-card');
+        return view('livewire.product-type-card');
     }
 }
