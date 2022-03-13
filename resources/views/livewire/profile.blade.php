45233a3b5e50ab6d9381a5189446b0ae8b2c8483
Mohammed Fadhil <mfanha8030@gmail.com>
2022-03-13 18:44:12 +0300

    feat: :sparkles: Implement update user profile

--- resources/views/livewire/profile.blade.php
+++ resources/views/livewire/profile.blade.php
@@ -11,16 +11,16 @@
                 </div>
             </div>
             <div class="mt-5 md:mt-0 md:col-span-2">
-                <form action="#" method="POST">
+                <form wire:submit.prevent='updateProfile("profileInfo")'>
                     <div class="shadow sm:rounded-md sm:overflow-hidden">
                         <div class="px-4 py-5 bg-white space-y-6 sm:p-6">
                             <div>
-                                <label class="block text-sm font-medium text-gray-700"> Photo </label>
+                                <label class="block text-sm font-medium text-dark-blue"> Photo </label>
                                 @if (Auth::user()->image != null)
                                     <div class="flex items-center">
                                         <img src="{{ Auth::user()->image }}" alt="User image" class="rounded-full">
                                         <button type="button"
-                                            class="ml-5 bg-white py-2 px-3 border border-gray-300 rounded-md shadow-sm text-sm leading-4 font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">Change</button>
+                                            class="ml-5 bg-white py-2 px-3 border border-gray-300 rounded-md shadow-sm text-sm leading-4 font-medium text-dark-blue hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 ">Change</button>
                                     </div>
                                 @else
                                     <div class="mt-1 flex items-center">
@@ -32,25 +32,27 @@
                                             </svg>
                                         </span>
                                         <button type="button"
-                                            class="ml-5 bg-white py-2 px-3 border border-gray-300 rounded-md shadow-sm text-sm leading-4 font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">Change</button>
+                                            class="ml-5 bg-white py-2 px-3 border border-gray-300 rounded-md shadow-sm text-sm leading-4 font-medium text-dark-blue hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 ">Change</button>
                                     </div>
                                 @endif
 
                             </div>
                             <div>
-                                <label for="about" class="block text-sm font-medium text-gray-700"> About </label>
+                                <label for="about" class="block text-sm font-medium text-dark-blue"> About </label>
                                 <div class="mt-1">
-                                    <textarea id="about" name="about" rows="3"
-                                        class="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 mt-1 block w-full sm:text-sm border border-gray-300 rounded-md text-black">{{ Auth::user()->bio }}</textarea>
+                                    <textarea wire:model.lazy='bio' id="about" name="about" rows="3"
+                                        class="shadow-sm   mt-1 block w-full sm:text-sm border border-gray-300 rounded-md text-black">{{ old('bio') }}</textarea>
                                 </div>
                                 <p class="mt-2 text-sm text-gray-500">Brief description for your profile.</p>
+                                @error('bio')
+                                    <p class="error">{{ $message }}</p>
+                                @enderror
                             </div>
 
 
                         </div>
                         <div class="px-4 py-3 bg-gray-50 text-right sm:px-6">
-                            <button type="submit"
-                                class="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">Save</button>
+                            <button type="submit" class="btn btn-primary transition">Save</button>
                         </div>
                     </div>
                 </form>
@@ -74,83 +76,116 @@
                 </div>
             </div>
             <div class="mt-5 md:mt-0 md:col-span-2">
-                <form action="#" method="POST">
+                <form wire:submit.prevent="updateProfile('personalInfo')">
                     <div class="shadow overflow-hidden sm:rounded-md">
                         <div class="px-4 py-5 bg-white sm:p-6">
                             <div class="grid grid-cols-6 gap-6">
                                 <div class="col-span-6 sm:col-span-3">
-                                    <label for="first-name" class="block text-sm font-medium text-gray-700">First
+                                    <label for="first-name" class="block text-sm font-medium text-dark-blue">First
                                         name</label>
-                                    <input type="text" name="first-name" id="first-name" autocomplete="given-name"
-                                        class="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
-                                        value="{{ Auth::user()->first_name }}">
+                                    <input wire:model.lazy='first_name' type="text" name="first_name" id="first-name"
+                                        autocomplete="given-name"
+                                        class="mt-1  focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
+                                        value="{{ old('first_name') }}">
+                                    @error('first_name')
+                                        <p class="error">{{ $message }}</p>
+                                    @enderror
                                 </div>
 
                                 <div class="col-span-6 sm:col-span-3">
-                                    <label for="last-name" class="block text-sm font-medium text-gray-700">Last
+                                    <label for="last_name" class="block text-sm font-medium text-dark-blue">Last
                                         name</label>
-                                    <input type="text" name="last-name" id="last-name" autocomplete="family-name"
-                                        class="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
-                                        value="{{ Auth::user()->last_name }}">
+                                    <input wire:model.lazy='last_name' type="text" name="last_name" id="last-name"
+                                        autocomplete="family-name"
+                                        class="mt-1  focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
+                                        value="{{ old('last_name') }}">
+                                    @error('last_name')
+                                        <p class="error">{{ $message }}</p>
+                                    @enderror
                                 </div>
 
                                 <div class="col-span-6 sm:col-span-4">
-                                    <label for="email-address" class="block text-sm font-medium text-gray-700">Email
+                                    <label for="email-address" class="block text-sm font-medium text-dark-blue">Email
                                         address</label>
-                                    <input type="text" name="email-address" id="email-address" autocomplete="email"
-                                        class="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
-                                        value="{{ Auth::user()->email }}">
+                                    <input wire:model.lazy='email' type="text" name="email" id="email-address"
+                                        autocomplete="email"
+                                        class="mt-1  focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
+                                        value="{{ old('email') }}">
+                                    @error('email')
+                                        <p class="error">{{ $message }}</p>
+                                    @enderror
                                 </div>
 
                                 <div class="col-span-6 sm:col-span-3">
-                                    <label for="country" class="block text-sm font-medium text-gray-700">Country</label>
-                                    <select id="country" name="country" autocomplete="country-name"
-                                        class="mt-1 block w-full py-2 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm">
-                                        <option value="{{ Auth::user()->address->id ?? null }}">
-                                            {{ Auth::user()->address->country ?? 'Select' }}</option>
+                                    <label for="country"
+                                        class="block text-sm font-medium text-dark-blue">Country</label>
+                                    <select wire:model.lazy='country' id="country" name="country"
+                                        autocomplete="country-name"
+                                        class="mt-1 block w-full py-2 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none  focus:border-indigo-500 sm:text-sm">
+                                        <option value="">Select</option>
+                                        <option value="{{ Auth::user()->address->country }}">
+                                            {{ Auth::user()->address->country }}</option>
+                                        <option>Iraq</option>
                                         <option>United States</option>
                                         <option>Canada</option>
                                         <option>Mexico</option>
                                     </select>
+                                    @error('country')
+                                        <p class="error">{{ $message }}</p>
+                                    @enderror
                                 </div>
 
                                 <div class="col-span-6">
-                                    <label for="street-address" class="block text-sm font-medium text-gray-700">Street
+                                    <label for="street-address" class="block text-sm font-medium text-dark-blue">Street
                                         address</label>
-                                    <input type="text" name="street-address" id="street-address"
-                                        autocomplete="street-address"
-                                        class="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
-                                        value="{{ Auth::user()->address->street ?? '' }}">
+                                    <input wire:model.lazy='street_address' type="text" name="street_address"
+                                        id="street-address" autocomplete="street-address"
+                                        class="mt-1  focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
+                                        value="{{ old('street_address') }}">
+                                    @error('street_address')
+                                        <p class="error">{{ $message }}</p>
+                                    @enderror
                                 </div>
 
                                 <div class="col-span-6 sm:col-span-6 lg:col-span-2">
-                                    <label for="city" class="block text-sm font-medium text-gray-700">City</label>
-                                    <input type="text" name="city" id="city" autocomplete="address-level2"
-                                        class="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
-                                        value="{{ Auth::user()->address->city ?? '' }}">
+                                    <label for="city" class="block text-sm font-medium text-dark-blue">City</label>
+                                    <input wire:model.lazy='city' type="text" name="city" id="city"
+                                        autocomplete="address-level1"
+                                        class="mt-1  focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
+                                        value="{{ old('city') }}">
+                                    @error('city')
+                                        <p class="error">{{ $message }}</p>
+                                    @enderror
                                 </div>
 
                                 <div class="col-span-6 sm:col-span-3 lg:col-span-2">
-                                    <label for="region" class="block text-sm font-medium text-gray-700">State /
+                                    <label for="region" class="block text-sm font-medium text-dark-blue">State /
                                         Province</label>
-                                    <input type="text" name="region" id="region" autocomplete="address-level1"
-                                        class="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
-                                        value="{{ Auth::user()->address->state ?? '' }}">
+                                    <input wire:model.lazy='state' type="text" name="state" id="region"
+                                        autocomplete="address-level1"
+                                        class="mt-1  focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
+                                        value="{{ old('state') }}">
+                                    @error('state')
+                                        <p class="error">{{ $message }}</p>
+                                    @enderror
                                 </div>
 
                                 <div class="col-span-6 sm:col-span-3 lg:col-span-2">
-                                    <label for="postal-code" class="block text-sm font-medium text-gray-700">ZIP /
+                                    <label for="postal-code" class="block text-sm font-medium text-dark-blue">ZIP /
                                         Postal
                                         code</label>
-                                    <input type="text" name="postal-code" id="postal-code" autocomplete="postal-code"
-                                        class="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
-                                        value="{{ Auth::user()->address->postal_code ?? '' }}">
+                                    <input wire:model.lazy='postal_code' type="text" name="postal_code" id="postal-code"
+                                        autocomplete="postal-code"
+                                        class="mt-1  focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
+                                        value="{{ old('postal_code') }}">
+                                    @error('postal_code')
+                                        <p class="error">{{ $message }}</p>
+                                    @enderror
                                 </div>
                             </div>
                         </div>
                         <div class="px-4 py-3 bg-gray-50 text-right sm:px-6">
-                            <button type="submit"
-                                class="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">Save</button>
+                            <button type="submit" class="btn btn-primary transition">Save</button>
                         </div>
                     </div>
                 </form>
