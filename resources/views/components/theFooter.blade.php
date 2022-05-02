d0c263765cc86f12c53f1bb0cd32eddc27b02764
Mohammed Fadhil <mfanha8030@gmail.com>
2022-05-02 17:18:02 +0300

    style: :lipstick: change footer style

--- resources/views/components/theFooter.blade.php
+++ resources/views/components/theFooter.blade.php
@@ -1,10 +1,9 @@
-<footer
-    class="p-4 mt-5 bg-primary text-white rounded-lg shadow md:px-6 md:py-8 {{ Route::is('garage.show') || Route::is('admin-dashboard') ? 'absolute left-0 bottom-0 w-full' : '' }}">
-    <div class="sm:flex sm:items-center sm:justify-between">
-        <a href="/" class="flex items-center mb-4 sm:mb-0">
-            <img src="{{ asset('assets/fav_icon/car_logo.svg') }}" class="h-8 mr-3 " alt="MCars Logo">
-            <span class="self-center text-2xl font-semibold whitespace-nowrap ">M CARS</span>
-        </a>
+<footer class="items-center p-4 mt-5 ds-footer bg-primary text-neutral-content">
+    <div class="items-center grid-flow-col">
+        <img src="{{ asset('assets/fav_icon/car_logo.svg') }}" alt="mcars logo">
+        <p>M Cars © 2022 - All right reserved</p>
+    </div>
+    <div class="grid-flow-col gap-4 md:place-self-center md:justify-self-end">
         <ul class="flex flex-wrap items-center mb-6 text-sm sm:mb-0 ">
             <li>
                 <a href="{{ route('explore.show') }}" class="mr-4 hover:underline md:mr-6 ">Explore</a>
@@ -20,8 +19,4 @@
             </li>
         </ul>
     </div>
-    <hr class="my-6 border-gray-200 sm:mx-auto lg:my-8">
-    <span class="block text-sm sm:text-center ">&copy; 2022 <a href="/" class="hover:underline">M
-            Cars</a>. All Rights Reserved.
-    </span>
 </footer>
